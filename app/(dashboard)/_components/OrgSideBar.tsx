beb90cb9b3c6df44a58745846554f745e5947ea4
noobDevUmar <m.umarqureshi091@gmail.com>
2024-02-17 00:26:52 -0800

    organiztion bar & switcher and buttons

--- app/(dashboard)/_components/OrgSideBar.tsx
+++ app/(dashboard)/_components/OrgSideBar.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { OrganizationSwitcher } from "@clerk/nextjs"
+import { LayoutDashboard, Star } from "lucide-react"
 import { Poppins } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
+import { useSearchParams } from "next/navigation"
 
 const font = Poppins({
   subsets:["latin"],
@@ -12,6 +15,11 @@ const font = Poppins({
 })
 
 const OrgSideBar = () => {
+
+const searchParams =   useSearchParams()
+  const favorites = searchParams.get("favorites")
+
+  
   return (
     <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 ">
       <Link href='/'>
@@ -32,7 +40,55 @@ const OrgSideBar = () => {
 
         </div>
       </Link>
-      <OrganizationSwitcher hidePersonal/>
+      <OrganizationSwitcher hidePersonal 
+      appearance={{
+        elements:{
+          rootBox:{
+            display:'flex',
+            justifyContent:"center",
+            alignItems:"center",
+            width:"100%"
+          },
+          organizationSwitcherTrigger:{
+
+            padding:'6px',
+            width:'100%',
+            borderRadius:"8px",
+            border:"1px solid #E5E7EB",
+            justifyContent:"space-between",
+            background:"white"
+          }
+        }
+      }}
+      />
+      <div className="space-y- w-full">
+      
+        <Button size="lg" asChild
+          variant={favorites ? "ghost" : "secondary"}
+          className="justify-start gap-1 px-2 w-full font-normal">
+          <Link href="/">
+            <LayoutDashboard />
+            Team Boards
+          </Link>
+
+        </Button>
+        <Button size="lg"
+          variant={favorites ? "secondary" : "ghost"}
+          asChild
+          className="justify-start gap-1 px-2 w-full font-normal">
+          <Link href={{
+            pathname: '/',
+            query: { favorites: true } // changes the url ?favorites=true
+          }}>
+            <Star />
+            Favorite Boards
+          </Link>
+
+        </Button>
+
+      </div>
+
+
         </div>
   )
 }
