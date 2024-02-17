7e8e677015f99b48ae9929dad06df021eb0305c9
noobDevUmar <m.umarqureshi091@gmail.com>
2024-02-17 03:21:19 -0800

    organization empty state addeD

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,19 @@
+"use client"
+import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/EmptyOrg";
 
 const DashBoardPage = ()=>{
 
-  return(
+  const { organization } = useOrganization(); // holds the active organization
+  
+  return (
     <div className=" p-6 flex-1 h-[calc(100%-80px)]">
-    <EmptyOrg/>
+
+      {!organization ? <EmptyOrg /> : (
+        <p>
+          Board
+        </p>
+      )}
     </div>
   )
 }
