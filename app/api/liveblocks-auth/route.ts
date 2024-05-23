9cc99e8e8018f0770737d67197de2e14186f836a
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2024-05-23 10:41:12 +0500

    working on authentication

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,55 @@
+import { Liveblocks } from "@liveblocks/node";
+import {auth ,currentUser} from "@clerk/nextjs"
+
+import { api } from "@/convex/_generated/api";
+import { ConvexHttpClient } from "convex/browser";
+
+const convex = new ConvexHttpClient(
+    process.env.NEXT_PUBLIC_CONVEX_URL!
+)
+const liveblocks = new Liveblocks({
+  secret: "sk_dev_gdrIT8HFkRmDBbb1ICsQ9FMtkXRI76OnJqvAavK5FtegS-WocN1JbAIUNGmxTEx4",
+});
+
+
+export async function POST(request: Request) {
+ 
+    const authorization = await auth();
+
+    const user = await currentUser();
+
+    if(!authorization || !user){
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    console.log("test");
+    
+
+    const {room} = await  request.json( )
+
+    const board = await convex.query(api.board.get,{id:room})
+
+    if(board?.orgId !== authorization.orgId){
+    throw new Response("Unauthorized", { status: 401 });
+    }
+
+    const userInfo={
+        name:user.firstName || "User",
+        picture:user.imageUrl
+    }
+
+
+    const session =  liveblocks.prepareSession(
+        user.id,
+        {userInfo}
+    );
+
+
+    if(room){
+        session.allow(room,session.FULL_ACCESS);
+
+        const {status,body} = await session.authorize();
+
+        return new Response(body,{status})
+    }
+}
--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values"
-import {mutation} from "./_generated/server"
+import {mutation, query} from "./_generated/server"
 
 const images=[
     "/placeholders/1.svg",
@@ -177,3 +177,14 @@ export const favorite = mutation({
       return board;
     },
   });
+
+
+  export const get = query({
+     args:{
+       id:v.id("boards") 
+     },
+     handler:async(ctx,args)=>{
+       const board = await ctx.db.get(args.id)
+       return board
+     }
+  })
