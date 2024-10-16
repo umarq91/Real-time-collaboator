0ba3e3f9cadb6dd11e668e6d8ab82bea0d9f6c49
Umar qureshi <m.umarqureshi091@gmail.com>
2024-10-16 13:41:03 +0500

    layer depth addeD

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -8,7 +8,7 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layer.s";
 import { Button } from "@/components/ui/button";
 import Hint from "@/components/hint";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -19,47 +19,103 @@ export const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
 
-const setFill = useMutation(
-  ({ storage }, fill: Color) => {
-    const liveLayers = storage.get("layers");
-    setLastUsedColor(fill);
- 
-    selection.forEach((id) => {
-      liveLayers.get(id)?.set("fill", fill);
-    });
-  },
-  [selection, setLastUsedColor]
-);
+      const moveToBack = useMutation(
+        ({ storage }) => {
+          const liveLayerIds = storage.get("layerIds");
+          const indices: number[] = [];
 
-  const deleteLayers = useDeleteLayers();
+          const arr = liveLayerIds.toArray();
+          for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+              indices.push(i);
+            }
+          }
+
+          for (let i = 0; i < indices.length; i++) {
+            liveLayerIds.move(indices[i], i);
+          }
+        },
+        [selection]
+      );
+
+
+      const moveToFront = useMutation(
+        ({ storage }) => {
+          const liveLayerIds = storage.get("layerIds");
+          const indices: number[] = [];
+
+          const arr = liveLayerIds.toArray();
+          for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+              indices.push(i);
+            }
+          }
+
+        
+          for(let i=indices.length -1 ;i>=0;i--){
+            liveLayerIds.move(
+              indices[i],
+              arr.length - 1 - (indices.length-1 -i)
+            )
+          }
+        },
+        [selection]
+      );
+    const setFill = useMutation(
+      ({ storage }, fill: Color) => {
+        const liveLayers = storage.get("layers");
+        setLastUsedColor(fill);
+
+        selection.forEach((id) => {
+          liveLayers.get(id)?.set("fill", fill);
+        });
+      },
+      [selection, setLastUsedColor]
+    );
+
+    const deleteLayers = useDeleteLayers();
     const selectionBounds = useSelectionBounds();
     if (!selectionBounds) return null;
 
     const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
     const y = selectionBounds.y + camera.y;
 
+    return (
+      <div
+        className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
+        style={{
+          transform: `translate(${x}px, ${y - 16}px) translate(-50%, -100%)`,
+        }}
+      >
+        <ColorPicker onChange={setFill} />
 
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label={"Bring to front"}>
+            <Button
+            onClick={moveToFront}
+            variant={"board"} size="icon">
+          <BringToFront/>
+            </Button>
+          </Hint>
 
+          <Hint label={"Send to back"}>
+            <Button 
+            onClick={moveToBack}
+            variant={"board"} size="icon">
+          <SendToBack/>
+            </Button>
+          </Hint>
+        </div>
 
-
-    return  <div
-    className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
-    style={{
-      transform: `translate(${x}px, ${y - 16}px) translate(-50%, -100%)`,
-    }}
-  >
-    <ColorPicker
-    onChange={setFill}
-    />
-    <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
-    <Hint label="Delete">
-    <Button variant={"board"} size="icon" onClick={deleteLayers}>
-<Trash2/>
-    </Button>
-
-    </Hint>
-    </div>
-    </div>;
+        <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
+          <Hint label="Delete">
+            <Button variant={"board"} size="icon" onClick={deleteLayers}>
+              <Trash2 />
+            </Button>
+          </Hint>
+        </div>
+      </div>
+    );
   }
 );
 
