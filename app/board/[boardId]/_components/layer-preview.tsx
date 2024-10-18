14ccef8514186629b70cd9ff79945c1abd4b9e38
Umar qureshi <m.umarqureshi091@gmail.com>
2024-10-18 11:43:21 +0500

    sticky note addeD

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -6,6 +6,7 @@ import { memo } from "react"
 import { Rectangle } from "./rectangle";
 import { Ellipse } from "./ellipse";
 import { Text } from "./text";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
   id: string;
@@ -23,6 +24,15 @@ export const LayerPreview = memo(({
   }
 
   switch (layer.type) {
+    case LayerType.Note:
+      return (
+        <Note
+        id={id}
+        layer={layer}
+        onPointerDown={onLayerPointerDown}
+        selectionColor={selectionColor}
+        />
+      )
     case LayerType.Text:
       return (
         <Text
--- lib/utils.ts
+++ lib/utils.ts
@@ -99,3 +99,9 @@ b:Point
   }
 return ids;
 }
+
+
+export function getContrastingTextColor(color:Color){
+  const liminance= 0.299 * color.r + 0.587 * color.g + 0.114 * color.b
+  return liminance > 182 ? "black" : "white"
+}
