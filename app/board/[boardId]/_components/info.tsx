4a4ee59b82522b9d9a4417b1bddb8ecae00ffdcf
Umar qureshi <m.umarqureshi091@gmail.com>
2024-11-01 16:15:09 +0500

    back addeD

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,11 +1,20 @@
 import { Skeleton } from "@/components/ui/skeleton"
+import Link from "next/link";
+import { IoReturnUpBackOutline } from "react-icons/io5";
+import {
+    Circle,
+    Undo2,
+  } from "lucide-react";
 
 export const Info = () => {
     return(
         <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12
         flex items-center shadow-md
         ">
-          TODO : Info about board
+            {/* TODO: Add info */}
+            <Link href={"/"} className="flex"> 
+            Go Back  <IoReturnUpBackOutline />
+            </Link>
         </div>
     )
 }
