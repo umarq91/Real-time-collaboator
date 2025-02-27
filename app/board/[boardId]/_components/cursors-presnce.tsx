42670f9aa5f9f1879e32dbf1cd83708efbfa0b28
Umar Qureshi <m.umarqureshi091@gmail.com>
2025-02-27 15:38:30 +0500

    finish the project

--- app/board/[boardId]/_components/cursors-presnce.tsx
+++ app/board/[boardId]/_components/cursors-presnce.tsx
@@ -3,8 +3,11 @@
 
 import {memo} from "react"
 
-import { useOthersConnectionIds } from "@/liveblocks.config";
+import { useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
 import { Cursor } from "./cursor";
+import { shallow } from "@liveblocks/client";
+import { Path } from "./paths";
+import { colorToCss } from "@/lib/utils";
 
 const Cursors = () => {
     const ids = useOthersConnectionIds();
@@ -21,10 +24,36 @@ const Cursors = () => {
         </>
     )
 }
+
+const Drafts = ()=>{
+    const others = useOthersMapped((other)=>({
+        pencilDraft:other.presence.pencilDraft,
+        penColor:other.presence.penColor
+    }),shallow);
+
+    return(
+        <>
+        {others.map(([key,other])=>{
+            if(other.pencilDraft){
+                return(
+                    <Path
+                    key={key}
+                    x={0}
+                    y={0}
+                    points={other.pencilDraft}
+                    fill={other.penColor ? colorToCss(other.penColor): '#000'}
+                    />
+                )
+            }
+            return null;
+        })}
+        </>
+    )
+}
 export const CursorsPresnce = memo(() => {
     return(
         <>
-        {/* Todo : Draft Pencil */}
+        <Drafts />
         <Cursors/>
         </>
     )
